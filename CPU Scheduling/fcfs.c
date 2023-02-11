788a350c5829b0f7660d21bf98fab38730178033
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-02-11 19:10:01 +0530

    FCFS without Arival Time

---
+++ CPU Scheduling/fcfs.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+// Fcfs Without arival time
+int main(){
+    int n;
+    printf("Enter the number of processes : ");
+    scanf("%d",&n);
+    int bt[n],wt[n],tat[n],twt,ttat;
+    printf("Enter the burst time of processes :\n");
+    for (int i = 0; i < n; i++){
+        scanf("%d",&bt[i]);
+    }
+    wt[0]=0;
+    for (int i = 1; i < n; i++){
+        wt[i] = wt[i-1]+bt[i-1];
+    }
+    twt=0;
+    ttat=0;
+    printf("---------------------------------------\n");
+    printf("| Process | waiting time |   ta time  |\n");
+    for (int i = 0; i < n; i++)
+    {
+        tat[i] = wt[i]+bt[i];
+        twt += wt[i];
+        ttat += tat[i];
+        printf("---------------------------------------\n");
+        printf("|    %d    |     %d        |    %d       |\n",i,wt[i],tat[i]);
+    }
+    printf("---------------------------------------\n");
+    printf("Average waiting time is %.2f",(float)twt/n);
+    printf("\nAverage turnaround time is %.2f\n",(float)ttat/n);
+    return 0;    
+}
